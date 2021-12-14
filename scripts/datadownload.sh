683fb811de5d15301c8893c4cf1e9968dfe53d22
Romane <romane.dorado-doncel@ens-lyon.fr>
2021-12-14 13:15:46 +0000

    add datadownload in scripts

--- scripts/datadownload.sh
+++ scripts/datadownload.sh
@@ -1,1 +1,3 @@
 wget -r -nH --user='igfl-UE_NGS_2021' --password='UE_NGS_2021'  ftp://sharegate-igfl.ens-lyon.fr/ -P data/
+#wget pour recupérer donner sur site web ici nos séquences sur le site de l'IGFL
+#-P pour mettre les données récupérées dans un dossier, ici data
