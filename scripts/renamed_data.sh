d2f7311e10321f014de716f81ecbed197d1afeac
Romane <romane.dorado-doncel@ens-lyon.fr>
2021-12-14 13:54:05 +0000

    renamed data

---
+++ scripts/renamed_data.sh
@@ -0,0 +1,13 @@
+cd data/fastq_files
+mv Lib10_62_21_S10_R1_001.fastq.gz 8c_+5_3.fastq.gz
+mv Lib11_62_21_S11_R1_001.fastq.gz 8c_+7_1.fastq.gz
+mv Lib12_62_21_S12_R1_001.fastq.gz 8c_+7_4.fastq.gz
+mv Lib1_62_21_S1_R1_001.fastq.gz 1c_2.fastq.gz
+mv Lib2_62_21_S2_R1_001.fastq.gz 1c_3.fastq.gz
+mv Lib3_62_21_S3_R1_001.fastq.gz 2c_2.fastq.gz
+mv Lib4_62_21_S4_R1_001.fastq.gz 2c_3.fastq.gz
+mv Lib5_62_21_S5_R1_001.fastq.gz 4c_1.fastq.gz
+mv Lib6_62_21_S6_R1_001.fastq.gz 4c_3.fastq.gz
+mv Lib7_62_21_S7_R1_001.fastq.gz 8c_1.fastq.gz
+mv Lib8_62-21_S8_R1_001.fastq.gz 8c_2.fastq.gz
+mv Lib9_62_21_S9_R1_001.fastq.gz 8c_+5_2.fastq.gz
